2f5ab39c8dae4bab49b1402b1c28d5a01f9bad2d
Faytey7 <Faithroberts708@gmail.com>
2023-03-03 04:37:31 +0100

    First Commit

---
+++ contracts/Token.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FTTOKEN is ERC20 {
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+    {
+        _mint(address(this), 100000 * (10**decimals()));
+    }
+}
---
+++ scripts/deploy.ts
@@ -0,0 +1,351 @@
+import { ethers } from "hardhat";
+import * as fs from "fs";
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";import dotenv from "dotenv";
+dotenv.config();
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+  const Token = await ethers.getContractFactory("FTTOKEN");
+  const token = await Token.deploy("FTToken", "FT2");
+  await token.deployed();
+
+  console.log(`Contract deployed to ${token.address}`);
+
+  const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_RPC);
+  const contractAddress = token.address;
+  const contractABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "name_",
+          "type": "string"
+        },
+        {
+          "internalType": "string",
+          "name": "symbol_",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Approval",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "Transfer",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        }
+      ],
+      "name": "allowance",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "approve",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+        {
+          "internalType": "uint8",
+          "name": "",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "subtractedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "decreaseAllowance",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "spender",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "addedValue",
+          "type": "uint256"
+        }
+      ],
+      "name": "increaseAllowance",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "transfer",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
+
+  const csvFile = fs.readFileSync("addresses.csv", "utf-8");
+  const csvs = csvFile.split("\n").splice(1);
+  console.log(`your csv is ${csvs}`);
+  
+  const values = await csvs.map(csv => csv.split(",")[1]?.trim());
+  console.log(`your values is ${values}`);
+
+  const notUndefined = values.filter(value => value !== undefined);
+
+  const leaves: any = notUndefined.map(value => ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["uint256"], [value])));
+  console.log(`your leaves are ${leaves}`);
+
+  const tree = StandardMerkleTree.of(leaves, ethers.utils.keccak256);
+  console.log(`your tree is ${JSON.stringify(tree)}`);
+
+  // const root = tree.getRoot();
+  const proof = tree.getProof(leaves[0]);
+
+  const treeData = {
+    leaves: leaves.map((leaf: { toHexString: () => any; }) => leaf.toHexString()),
+    root: root.toHexString(),
+    depth: tree.getDepth()
+  };
+  fs.writeFileSync('merkle-tree.json', JSON.stringify(treeData));
+  fs.writeFileSync('merkle-proof.json', JSON.stringify(proof));
+
+  const contract = new ethers.Contract(contractAddress, contractABI, owner);
+
+  const conRoot = await contract.setRoot(root);
+
+  console.log(`contract root is ${conRoot}`);
+
+  const exampleAddress = '0x9bF599C7BC72c4fB4fE2C4CE0e7bBca8541d61f9';
+const exampleValue = values[0]; // Value corresponding to example address
+const exampleLeaf = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['uint256'], [exampleValue]));
+const exampleProof = tree.getProof(exampleLeaf);
+const exampleResult = await contract.claim(exampleAddress, exampleValue, exampleProof);
+console.log('Example claim result:', exampleResult);
+  
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
